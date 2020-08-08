196125d25000c5a4f01231de4bcc6bcc40c55ceb
seanngpack <seanngpack@gmail.com>
2020-08-08 11:06:39 -0400

    added cmakelists

---
+++ include/stuffs/swag.h
@@ -0,0 +1,10 @@
+//
+// Created by Sean ng pack on 8/8/20.
+//
+
+#ifndef FEELING_BLUE_SWAG_H
+#define FEELING_BLUE_SWAG_H
+
+void print_swag();
+
+#endif //FEELING_BLUE_SWAG_H
---
+++ src/stuffs/swag.cpp
@@ -0,0 +1,1 @@
+#include "swag.h"
